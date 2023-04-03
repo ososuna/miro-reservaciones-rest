757c027812b7554d5dc15807febf9662886027e0
oswi <oswaldo.adrian@live.com.mx>
2023-04-03 12:50:36 -0700

    first commit

---
+++ src/main/java/dev/ososuna/miro/MiroApplication.java
@@ -0,0 +1,13 @@
+package dev.ososuna.miro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ososuna/miro/MiroApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ososuna.miro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
