dc7c1e86c1c606270150f7cadda18c8627b518df
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2023-04-23 15:49:07 -0600

    added unique date and time validation for reserve

--- src/main/java/dev/ososuna/miro/controller/ReservationController.java
+++ src/main/java/dev/ososuna/miro/controller/ReservationController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.ososuna.miro.exception.BadRequestException;
 import dev.ososuna.miro.exception.NotFoundException;
 import dev.ososuna.miro.model.Reservation;
 import dev.ososuna.miro.model.dto.AvailableHourDto;
@@ -25,7 +26,7 @@ public class ReservationController {
   private final ReservationService reservationService;
 
   @PostMapping
-  public ResponseEntity<Reservation> reserve(@RequestBody ReserveRequestDto request) throws NotFoundException {
+  public ResponseEntity<Reservation> reserve(@RequestBody ReserveRequestDto request) throws NotFoundException, BadRequestException {
     return ResponseEntity.status(HttpStatus.CREATED).body(reservationService.reserve(request));
   }
 
