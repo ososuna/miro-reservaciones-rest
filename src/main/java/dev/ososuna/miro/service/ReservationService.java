7fc81ca425640b6e67d2c741c85e26d2811e35c4
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2023-04-24 17:02:10 -0600

    refactor get total available hours

--- src/main/java/dev/ososuna/miro/service/ReservationService.java
+++ src/main/java/dev/ososuna/miro/service/ReservationService.java
@@ -2,12 +2,15 @@ package dev.ososuna.miro.service;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import dev.ososuna.miro.exception.BadRequestException;
 import dev.ososuna.miro.exception.NotFoundException;
 import dev.ososuna.miro.model.Reservation;
+import dev.ososuna.miro.model.dto.AvailableHourDto;
 import dev.ososuna.miro.model.dto.AvailableHoursResponseDto;
 import dev.ososuna.miro.model.dto.ReserveRequestDto;
 import dev.ososuna.miro.repository.ReservationRepository;
@@ -26,7 +29,24 @@ public class ReservationService {
   private final ReservationRepository reservationRepository;
 
   public AvailableHoursResponseDto getAvailableHours() {
-    return reservationUtil.getTotalAvailableHoursForToday();
+    List<AvailableHourDto> availableHours = new ArrayList<>();
+    var currentDate = LocalDate.now();
+    var now = LocalTime.now();
+    var start = LocalTime.of(6, 0);
+    var end = LocalTime.of(23, 0);
+    while (start.isBefore(end)) {
+      if (start.isAfter(now)) {
+        var exists = reservationRepository.existsByDayAndStartTimeAndEndTime(currentDate, start, start.plusHours(1));
+        if (!exists) {
+          availableHours.add(new AvailableHourDto(start.toString(), start.plusHours(1).toString()));
+        }
+      }
+      start = start.plusHours(1);
+    }
+    return AvailableHoursResponseDto.builder()
+      .day(currentDate.toString())
+      .availableHours(availableHours)
+      .build();
   }
 
   public Reservation reserve(ReserveRequestDto request) throws NotFoundException, BadRequestException {
